80d72beeaf9a18e81c4d8759e5239640fef4db9a
Puscasu Alexandru <palexandru2000@gmail.com>
2022-04-22 11:18:22 +0300

    test for add entity and attribute actions

---
+++ src/test/java/org/example/ipdp/proiect/backend/ActionsTestOnBackend.java
@@ -0,0 +1,50 @@
+package org.example.ipdp.proiect.backend;
+
+import org.example.ipdp.proiect.actions.*;
+import org.example.ipdp.proiect.misc.Attribute;
+import org.example.ipdp.proiect.misc.DataModel;
+import org.example.ipdp.proiect.misc.Entity;
+import org.example.ipdp.proiect.misc.IAction;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ActionsTestOnBackend {
+    @Test
+    public void addNewEntityByAction()
+    {
+        Backend backend = new Backend();
+        BackendHelper backendHelper = new BackendHelper(backend);
+
+        String entityName = "entity1";
+        IAction action = new AddEntityAction(new Entity(entityName));
+
+        action.applyStorage(backend);
+
+        assertNotNull(backendHelper.getEntityByName(entityName));
+    }
+
+    @Test
+    public void addNewAttributeByAction()
+    {
+        Backend backend = new Backend();
+        BackendHelper backendHelper = new BackendHelper(backend);
+
+        String parentName = "entity1";
+        Entity parent = new Entity(parentName);
+
+        backend.addEntity(parent);
+
+        String attrName = "attr1";
+        Attribute attribute = new Attribute(attrName, "type1");
+        IAction action = new AddAttributeAction(parentName, attribute);
+
+        action.applyStorage(backend);
+
+        List<org.example.ipdp.proiect.backend.orm.Attribute> attrs = backendHelper.getEntityAttributes(parentName);
+
+        assertEquals(1, attrs.size());
+    }
+}
