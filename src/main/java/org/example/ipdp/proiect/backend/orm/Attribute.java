6c2340d77f8d938927e2723e9bab397210cd2998
Alexandru Puscasu <palexandru2000@gmail.com>
2022-04-22 11:21:19 +0300

    add cayenne orm

---
+++ src/main/java/org/example/ipdp/proiect/backend/orm/Attribute.java
@@ -0,0 +1,9 @@
+package org.example.ipdp.proiect.backend.orm;
+
+import org.example.ipdp.proiect.backend.orm.auto._Attribute;
+
+public class Attribute extends _Attribute {
+
+    private static final long serialVersionUID = 1L; 
+
+}
