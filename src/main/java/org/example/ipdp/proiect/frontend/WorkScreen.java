424e231366589662059ba4e9f21b8ca3096ce9d0
Puscasu Alexandru <palexandru2000@gmail.com>
2022-05-08 17:22:17 +0300

    implement add and remove entity actions

--- src/main/java/org/example/ipdp/proiect/frontend/WorkScreen.java
+++ src/main/java/org/example/ipdp/proiect/frontend/WorkScreen.java
@@ -1,10 +1,28 @@
 package org.example.ipdp.proiect.frontend;
 
+import org.example.ipdp.proiect.misc.DataModel;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
 
 public class WorkScreen extends State{
     JTabbedPane entities;
+    DataModel data = null;
+
+    public WorkScreen() {
+        data = new DataModel();
+    }
+
+    protected void addEntity(String entityName) {
+        entities.addTab(entityName, new EntityPanel(entityName));
+    }
+
+    protected void removeEntity(String entityName) {
+        entities.removeTabAt(entities.indexOfTab(entityName));
+    }
 
     @Override
     protected void userInit() {
@@ -23,10 +41,55 @@ public class WorkScreen extends State{
         buttonsRaw.add(removeEntity);
         buttonsRaw.add(closeProjectButton);
 
+        addEntityButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String entityName = (String)JOptionPane.showInputDialog(
+                        frame,
+                        "New entity name:",
+                        "Add entity",
+                        JOptionPane.PLAIN_MESSAGE,
+                        null,
+                        null,
+                        null
+                );
+
+                if (entityName != null && entityName.length() > 0) {
+                    addEntity(entityName);
+                }
+            }
+        });
+
+        removeEntity.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int count = entities.getTabCount();
+                Vector<String> possibilities = new Vector<>(count);
+
+                for (int i = 0; i < count; ++i) {
+                    possibilities.add(entities.getTitleAt(i));
+                }
+
+                String entityName = (String)JOptionPane.showInputDialog(
+                        frame,
+                        "Remove entity:",
+                        "Remove entity",
+                        JOptionPane.PLAIN_MESSAGE,
+                        null,
+                        possibilities.toArray(),
+                        null
+                );
+
+                if (entityName != null && entityName.length() > 0) {
+                    removeEntity(entityName);
+                }
+            }
+        });
+
         entities = new JTabbedPane();
-        entities.addTab("entity1", new EntityPanel("entity1"));
-        entities.addTab("entity2", new EntityPanel("entity2"));
-        entities.addTab("entity2", new EntityPanel("entity2"));
+        //entities.addTab("entity1", new EntityPanel("entity1"));
+        //entities.addTab("entity2", new EntityPanel("entity2"));
+        //entities.addTab("entity2", new EntityPanel("entity2"));
 
         frame.setLayout(new BorderLayout());
         frame.add(buttonsRaw, BorderLayout.NORTH);
